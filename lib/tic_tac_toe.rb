6da046582036fbfa1a26a13a0abbf563e1416542
Stela Capsa <stela.capsa@gmail.com>
2020-01-04 14:57:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,8 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(index)
-      move(index, current_player)
+      index
+      current_player
       display_board
     else
       turn
