99aa6dfb217ad06c8919df9064475e67317dbc99
Stela Capsa <stela.capsa@gmail.com>
2020-01-04 13:55:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,4 +12,24 @@ class TicTacToe
      [1,4,8]
      [3,4,6]
      ]
+  
+  
+  def display_board
+    puts "#{@board[0]}, #{@board[1]}, #{@board[2]}"
+    puts"------------"
+    puts "#{@board[3]}, #{@board[4]}, #{@board[5]}"
+    puts"------------"
+    puts "#{@board[6]}, #{@board[7]}, #{@board[8]}"
+
+  end
+  
+  def input_to_index(input)
+    input.to_i -1
+    
+  end
+   
+   
+   
+   
+     
 end
