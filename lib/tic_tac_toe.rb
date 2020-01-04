739f14b38fdc711866a5e24a36dbe0e91a8c0f6e
Stela Capsa <stela.capsa@gmail.com>
2020-01-04 14:13:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,15 @@ class TicTacToe
   def move(index, token = "X")
     @board[index] = token
   end   
+  
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end 
+  
+  
+  def valid_move?(input)
+    index.between?(0,8) && !position_taken(index)
+  end
    
    
    
