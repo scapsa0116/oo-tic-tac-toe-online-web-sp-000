d952f69f5cb58405ac3cf1a716d10ecbfa0cedaf
Stela Capsa <stela.capsa@gmail.com>
2020-01-04 13:59:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,7 @@ class TicTacToe
     @board = board || Aray.new(9, " ")
   end
    
-def WIN_COMBINATIONS  
+ 
    WIN_COMBINATIONS = [
      [1,2,3],
      [3,4,5],
@@ -15,7 +15,7 @@ def WIN_COMBINATIONS
      [2,4,7],
      [3,5,8]
      ]
-  end
+
   
   def display_board
     puts "#{@board[0]}, #{@board[1]}, #{@board[2]}"
