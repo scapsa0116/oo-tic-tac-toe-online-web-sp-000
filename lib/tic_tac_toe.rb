8605da92e08eddc30fddef6d21101f9c7569570f
Stela Capsa <stela.capsa@gmail.com>
2020-01-04 14:16:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,6 +43,10 @@ class TicTacToe
   def valid_move?(input)
     index.between?(0,8) && !position_taken(index)
   end
+  
+  def turn_count
+    @board.each {|token| token == "X" || token == "O"}
+  end
    
    
    
